765b2470da4cb417c322129f1975182b4c61332b
Nodirbek Kadamov <kadamovnk@icloud.com>
2024-01-26 16:07:29 +0500

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by Nodirbek Kadamov on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
